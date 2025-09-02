714da48c234313e81b5ac43b736ad543aa75fab3
Chlod Alejandro <chlod@chlod.net>
2025-09-02 23:20:45 +0800

    fix display

--- src/embeds/embed.ts
+++ src/embeds/embed.ts
@@ -7,7 +7,7 @@ export function embed(): EmbedBuilder {
         .setFooter({
             text: `relatime ${
                 version ?
-                    `v${version}${process.env.NODE_ENV !== "production" ? "" : "-dev"}` :
+                    `v${version}${process.env.NODE_ENV !== "production" ? "-dev" : ""}` :
                     "unknown version"
             } | \u{1F6E0} \u{fe0f}\u{2764} @chlod`,
             iconURL: "https://cdn.discordapp.com/app-icons/1405466156063522937/a51d86268668c5fd8d07d4e9bb444043.png?size=256",
